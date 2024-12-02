309ad8698a9987e19a98d0750652148ab07b4ed1
Ghadachihi99 <ghada.chihi@outlook.com>
2024-12-02 17:57:02 +0100

    raja3na el rg fel managed

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-
+#just creating conflict
 resource "azurerm_virtual_network" "example" {
   name                = "myVnet-${var.environment}"
   resource_group_name = azurerm_resource_group.example.name
